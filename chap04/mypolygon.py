d6185efb6c0a638be337da0ce557c0e9c0c1cb7d
JLangowitz <joshua.langowitz@students.olin.edu>
2013-09-24 11:46:06 -0400

    docstrings

--- chap04/mypolygon.py
+++ chap04/mypolygon.py
@@ -13,21 +13,45 @@ bob.delay=.01
 print bob
 
 def square(t):
+	'''
+	Draws a square of side length 100
+
+	t: turtle object to move
+	'''
 	for i in xrange(4):
 		fd(t,100)
 		lt(t)
 
 def polygon(t,n):
+	'''
+	Draws a polygon of side length 100 with n sides
+
+	t: turtle object to move
+	'''
 	for i in xrange(n):
 		fd(t,100)
 		lt(t,360.0/n)
 
 def circle(t,r):
+	'''
+	Draws a hectohedron approximation of a circle with radius r
+
+	t: turtle object to move
+	r: radius of circle
+	'''
 	for i in xrange(100):
 		fd(t,2*pi*r/100)
 		lt(t,3.6)
 
 def arc(t,r,angle):
+	'''
+	Draws an arc of angle of the hectohedron approximation 
+	of a circle with radius r
+
+	t: turtle object to move
+	r: radius of circle
+	angle: angle of arc to move
+	'''
 	for i in xrange(int(100*angle/360.0)):
 		fd(t,2*pi*r/100)
 		lt(t,3.6)
